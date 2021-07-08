ce5ec199b97fe8d5ebe4584d218f51c63626b261
Daniel Greidinger <daniel@farm-ng.com>
2021-07-08 14:25:24 -0400

    Win build changes

--- example/apriltag_demo.c
+++ example/apriltag_demo.c
@@ -34,7 +34,7 @@ either expressed or implied, of the Regents of The University of Michigan.
 #include <stdint.h>
 #include <inttypes.h>
 #include <ctype.h>
-#include <unistd.h>
+//#include <unistd.h>
 #include <math.h>
 
 #include "apriltag.h"
@@ -115,7 +115,7 @@ int main(int argc, char *argv[])
 
     int maxiters = getopt_get_int(getopt, "iters");
 
-    const int hamm_hist_max = 10;
+ #define hamm_hist_max  10
 
     for (int iter = 0; iter < maxiters; iter++) {
 
